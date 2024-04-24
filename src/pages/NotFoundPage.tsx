f6be5245cf9c0630f21ef71ba6318c9c08d65de8
Pedro <pedrojuniorifpb@gmail.com>
2024-04-24 18:48:03 -0300

    Fix visual bug in NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,13 +2,11 @@ import { Link } from "react-router-dom";
 
 export function NotFoundPage() {
   return (
-    <div className="flex-row flex-1 p-4 text-center">
-      <div className="items-center justify-center relative flex-1 gap-6">
-        <div>
-          <h1 className="text-3xl">Não encontramos a página</h1>
-          <div>Por favor confira se o endereço buscado está correto</div>
-        </div>
-        <Link to="/" className="default-submit px-4">
+    <div className="h-screen p-4 text-center content-center bg-primary-50">
+      <div className="flex flex-col gap-1.5">
+        <h1 className="text-3xl">Não encontramos a página</h1>
+        <div>Por favor confira se o endereço buscado está correto</div>
+        <Link to="/" className="text-primary-700">
           Ir para a tela inicial
         </Link>
       </div>
