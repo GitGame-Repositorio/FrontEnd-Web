d9544d969b2c722a89c096450a0bfe67a77e309c
Pedro <pedrojuniorifpb@gmail.com>
2024-07-08 20:06:11 -0300

    Modify and change colors for icons in menu action

--- src/common/menuAction/MenuActionUser.tsx
+++ src/common/menuAction/MenuActionUser.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
-import { GoGear } from "react-icons/go";
-import { MdExitToApp } from "react-icons/md";
-import { LuLayoutDashboard } from "react-icons/lu";
+import { MdOutlineSettings } from "react-icons/md";
+import { LuLayoutDashboard, LuLogOut } from "react-icons/lu";
 import { LuSaveAll } from "react-icons/lu";
 
 import theme from "../../service/tailwindTheme";
@@ -13,12 +12,24 @@ export const MenuActionUser = () => {
   const styleItemMenu =
     "flex gap-2 items-center rounded-md p-1.5 hover:bg-primary-300";
 
+  const colorIcons = theme.colors.primary[600];
+
   return (
     <div className="absolute top-18 right-0 min-w-50 z-10 flex flex-col text-start gap-2 py-4 px-3 font-bold bg-primary-200 rounded-2xl border border-solid border-primary-500 text-primary-800">
+      {!isLogged && (
+        <>
+          <Link to="/add-email" onClick={() => {}} className={styleItemMenu}>
+            <LuSaveAll size={22} color={colorIcons} />
+            Salvar Progresso
+          </Link>
+          <hr className="h-px w-full bg-primary-500" />
+        </>
+      )}
+
       {isAdmin && user?.admin?.canManageContentGame && (
         <>
           <Link to="/dashboard" className={styleItemMenu}>
-            <LuLayoutDashboard size={22} color={theme.colors.primary[500]} />
+            <LuLayoutDashboard size={22} color={colorIcons} />
             Dashboard
           </Link>
           <hr className="h-px w-full bg-primary-500" />
@@ -26,23 +37,13 @@ export const MenuActionUser = () => {
       )}
 
       <Link to="/user" className={styleItemMenu}>
-        <GoGear size={22} color={theme.colors.primary[500]} />
+        <MdOutlineSettings size={22} color={colorIcons} />
         Configurações
       </Link>
       <hr className="h-px w-full bg-primary-500" />
 
-      {!isLogged && (
-        <>
-          <Link to="/add-email" onClick={() => {}} className={styleItemMenu}>
-            <LuSaveAll size={22} color={theme.colors.primary[500]} />
-            Salvar Remoto
-          </Link>
-          <hr className="h-px w-full bg-primary-500" />
-        </>
-      )}
-
       <Link to="/" onClick={logout} className={styleItemMenu}>
-        <MdExitToApp size={22} color={theme.colors.primary[500]} />
+        <LuLogOut size={22} color={colorIcons} />
         {isLogged ? "Sair" : "Sair e Resetar"}
       </Link>
     </div>
